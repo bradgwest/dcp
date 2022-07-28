92243b8bf2dc39a3d3635ce488a2e358bede8826
Brad West <brad.g.west@gmail.com>
2022-07-28 07:43:22 -0600

    2022-07-28

---
+++ dcp/problems/book/10/10_1.py
@@ -0,0 +1,26 @@
+def search(graph, vertex, visited, parent) -> bool:
+    """dfs returning boolean if there's a cycle for the current start node"""
+    visited[vertex] = True
+
+    for neighbor in graph[vertex]:
+        if not visited[neighbor]:
+            if search(graph, neighbor, visited, vertex):
+                return True
+        elif parent != neighbor:
+            return True
+
+    return False
+
+
+def has_cycle(graph):
+    visited = {v: False for v in graph.keys()}
+
+    for vertex in graph.keys():
+        if not visited[vertex]:
+            if search(graph, vertex, visited, None):
+                return True
+    return False
+
+
+graph = {1: [2, 3], 2: [4], 3: [], 4: []}
+print(has_cycle(graph))
